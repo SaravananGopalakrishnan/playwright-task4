e9b82d26d148cd67183d5cc09c539041454a24b1
SaravananGopalakrishnan <u517968@wns.com>
2026-06-08 16:06:40 +0530

    Added QA Playground smoke test

---
+++ tests/qaplayground.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "@playwright/test";
+
+test("QA Playground Home Page", async ({ page }) => {
+  await page.goto("https://qaplayground.dev/");
+  await expect(page).toHaveTitle(/QA Playground/);
+});
